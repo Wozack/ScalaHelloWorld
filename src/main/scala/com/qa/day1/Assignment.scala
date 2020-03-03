224b3c90ad5f6c3b595d4b6a27f47367f2e0cb30
Wozack <wozniakjack96@gmail.com>
2020-03-03 09:16:59 +0000

    List manipulation added

---
+++ src/main/scala/com/qa/day1/Assignment.scala
@@ -0,0 +1,10 @@
+package com.qa
+
+package object day1 {
+  def main(args: Array[String]): Unit = {
+    stringPrint("AAAAAAAAAAAAAAAAAAAAAAAAAAAA")
+  }
+  def stringPrint(a:String): String ={
+    a
+  }
+}
---
+++ src/main/scala/com/qa/day1/NumWorker.scala
@@ -0,0 +1,17 @@
+package com.qa
+
+package object NumWorker extends App {
+//  def main(args: Array[Int]): Int = {
+//    numWorker(2, 8, 34)
+//  }
+//
+//  def numWorker(A: Int, B: Int, C: Int): Int = {
+//    val list = List(A, B, C)
+//
+//    val newList = list.sorted
+//
+//    val sum = (newList(1) + newList(2)) * newList(0)
+//
+//    sum
+//  }
+}
