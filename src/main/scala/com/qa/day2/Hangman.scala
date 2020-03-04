fe56389ba5cface456221f3d595f5c3505f23816
Wozack <wozniakjack96@gmail.com>
2020-03-04 11:47:26 +0000

    improved hangman, finished iteration

--- src/main/scala/com/qa/day2/Hangman.scala
+++ src/main/scala/com/qa/day2/Hangman.scala
@@ -9,35 +9,48 @@ object Hangman {
   val arr = ArrayBuffer[String]()
   val random = new scala.util.Random
   val num = random.nextInt(101)
-  val word = arr(num).toCharArray
-  var hiddenWord = "_ " * word.length
+  var word = ""
+  var hiddenWord = ""
+  var deathCounter = 0
+
 
   def main(args: Array[String]): Unit = {
-
     hangmanWord(true)
+    enterLetter(true)
   }
 
-  def hangmanWord(bool: Boolean): Unit ={
+  def hangmanWord(bool: Boolean): Unit = {
 
 
     val filename = "hangman100.txt"
     for (line <- Source.fromFile(filename).getLines()) {
-          arr.+=(line)
-        }
+      arr.+=(line)
+    }
+    word = arr(num)    //.toCharArray
 
-
+    hiddenWord = "_ " * word.length()
     println(hiddenWord)
-
+    println(word)
   }
 
   def enterLetter(bool: Boolean): Unit ={
 
     println("Enter a letter for the Hangman: ")
     val ltr = scala.io.StdIn.readChar()
+    hiddenWord = ""
     if(word.contains(ltr)){
       println(s"You are correct, $ltr is in the Hangman's Word...")
-
+      for(i <- 0 to word.length-1 by 1){
+        if(ltr != word(i)){
+          hiddenWord += "_ "
+        }else{
+          hiddenWord += ltr + " "
+        }
+      }
+    }else{
+      println("THAT IS INCORRECT! DEATH DRAWS CLOSER!")
+      deathCounter += 1
     }
-
+    println(hiddenWord)
   }
 }
--- src/main/scala/com/qa/day2/Iteration.scala
+++ src/main/scala/com/qa/day2/Iteration.scala
@@ -1,5 +1,16 @@
 package com.qa
 
-package object Iteration {
+object Iteration {
 
+  def main(args: Array[String]): Unit = {
+    iterator("Y", 5)
+  }
+
+  def iterator(str:String, int: Int): Unit = {
+
+    for (i <- 0 to int-1 by 1){
+      println(str*int)
+
+    }
+  }
 }
